57bb851a8880f2e75c75e4dde596a3d7759cc2ef
Jackson Cabanatuan <JacksonCabanatuan@jacksons-air.attlocal.net>
2019-04-04 20:55:07 -0700

    added buttons

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ $(function() {
   $("button").click(function(event) {
     const listItem = document.getElementById('inputItem').value;
     $("section").append(
-      `<div class="list-item"><p>${listItem}</p></div>`
+      `<div class="list-item"><p>${listItem}</p></div>` +
+      `<div class ="list-options"><button type="button" id ="check">check</button><button type="button" id ="delete">delete</button></div>`
       //append text from the input in a paragraph. Add it to section
     );
   });
